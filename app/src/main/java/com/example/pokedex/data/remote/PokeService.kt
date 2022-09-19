749b0e99716f494e456ffae7a4ba4427e937b62d
gr1269 <gr12@bk.ru>
2022-09-19 17:28:26 +0600

    Load Screenshots

--- app/src/main/java/com/example/pokedex/data/remote/PokeService.kt
+++ app/src/main/java/com/example/pokedex/data/remote/PokeService.kt
@@ -3,6 +3,7 @@ package com.example.pokedex.data.remote
 import com.example.pokedex.domain.Pokemon
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface PokeService {
     @GET("pokemon.json")
